91875a5a99448dad15b6edbb633e4172642148f6
Viji <Vijigold02@gmail.com>
2021-12-14 23:40:00 +0530

    Updated code

--- python.py
+++ python.py
@@ -1,4 +1,9 @@
 X = [1,2,3,4,5,6,7]
+number = int(input("Enter a number :")
 
 for num in X:
  print (num)
+ if num == number:
+  print ("Found the number : {}".format(num)
+ else:
+  print ("Did not find the number")
